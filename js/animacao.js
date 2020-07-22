c03e2f042ae937e3ab0b2ed51cb77b207cbee38e
Thiago Silva <othiagosilva.dev@gmail.com>
2020-07-21 22:12:09 -0300

    Add files via upload

---
+++ js/animacao.js
@@ -0,0 +1,34 @@
+var minhaImagem = document.querySelector(".minha-imagem");
+setTimeout(function(){
+	minhaImagem.classList.add("mostra-item");
+}, 300);
+
+var titulo = document.querySelector(".titulo");
+setTimeout(function(){
+	titulo.classList.add("mostra-item");
+}, 400);
+
+var textoApresentacao = document.querySelector(".apresentacao");
+setTimeout(function(){
+	textoApresentacao.classList.add("mostra-item");
+}, 500);
+
+var listaSkills = document.querySelector(".skills");
+setTimeout(function(){
+	listaSkills.classList.add("mostra-item");
+}, 600);
+
+var listaDiferenciais = document.querySelector(".diferentials");
+setTimeout(function(){
+	listaDiferenciais.classList.add("mostra-item");
+}, 700);
+
+var imagemCerebro = document.querySelector(".imagem-cerebro");
+setTimeout(function(){
+	imagemCerebro.classList.add("mostra-item");
+}, 800);
+
+var imagemHeadphone = document.querySelector(".imagem-headphone");
+setTimeout(function(){
+	imagemHeadphone.classList.add("mostra-item");
+}, 500);
